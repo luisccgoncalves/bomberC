f3a4698c21f4cd8cd6b377dc8c835f579e138ea1
luisccgoncalves <luisccgoncalves@gmail.com>
2017-11-28 17:59:23 +0000

    Meta 1

---
+++ structs.h
@@ -0,0 +1,54 @@
+#ifndef STRUCTS_H
+#define STRUCTS_H
+
+#define USR_TAM     50      //defines user string sizes
+#define USR_LINE    1024    //defines whole line max size
+#define LVL_W       20      //map width
+#define LVL_H       30      //map height
+
+typedef struct {    //sent from client to server with auth request
+    int     pid;            //creates a unique pipe to callback
+    char    user[USR_TAM];
+    char    passwd[USR_TAM];
+}user;
+
+typedef struct {    //used to create an array usernames and password
+    char    user[USR_TAM];
+    char    passwd[USR_TAM];
+}db;
+
+typedef struct{
+    int     x_pos;      //bomb x position
+    int     y_pos;      //bomb y position
+    int     bomb_power; //2 or 4 squares of range minreq
+    unsigned long fuse; //timestamp of bomb placement, fuse goes off after 2s
+}bomb;
+
+typedef struct{         //enemy only has x y position
+    int     x_pos;
+    int     y_pos;
+}enemy;
+
+typedef struct{
+    char    user[USR_TAM];
+    int     x_pos;      //player x position
+    int     y_pos;      //player y position
+    int     n_bombs;    //number of bombs
+    int     n_bobombs;  //number of big bombs
+    int     points;     //player points
+}bomber;
+
+typedef struct{         //object only has x y position
+    int     x_pos;
+    int     y_pos;
+}object;
+
+typedef struct{
+    char    terrain[LVL_W][LVL_W];
+    int     n_obj;      //number of tokens to collect
+    int     n_enemies;  //number of enemies
+    int     exit[2];    //exit x y position, enabled when n_obj=0
+}level;
+
+
+#endif /* STRUCTS_H */
